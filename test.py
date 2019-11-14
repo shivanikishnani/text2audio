349866e31459a778fdffef8935752c0c1554530f
Aditya Sengupta <aditya.sengupta@berkeley.edu>
2019-11-14 01:21:37 -0800

    hello world yay but 3x to slow. Holds up to music but not to clapping

--- test.py
+++ test.py
@@ -4,14 +4,19 @@ import itertools
 from time import sleep
 
 test_chars = 1000 # test parameter: number of characters you want to hear.
-lead = 0.1 # experimental parameter: the observed time taken by garbage characters before message starts.
+lead = 0.2 # experimental parameter: the observed time taken by garbage characters before message starts.
 lag = 0.25 # experimental parameter: the observed time by which listening lags behind hearing the message.
 show = False # if you want to show the message being sent and received in parallel
 
+lead = 0
+
 if __name__ == "__main__":
     stream, audio = start_listening()
-    message = "the five boxing wizards jump quickly."
+    message = "A regrade request has been submitted for Justin on Question 3.2: (b) (from Homework 6): no points, but I got the right answer."
     received = []
+
+    for _ in range(int(lead / CHARTIME)):
+        read_from_stream(stream, CHARTIME)    
 
     for char in message[:test_chars]:
         # I feel like there might be a bug here because of synchronization: it might stop listening before I start playing.
@@ -31,5 +36,5 @@ if __name__ == "__main__":
             print(' ', decode_frame(heard))
 
     stop_listening(stream, audio)
-    print(''.join(received))
+    print(''.join(received)[1:])
     
