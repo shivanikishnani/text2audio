db6bd6175c13e56682ca5837bf7571be6e7bf628
Briana Zhang <b_zhang@berkeley.edu>
2019-12-04 09:51:34 -0800

    written rudimentary new encoding

---
+++ encoder.py
@@ -0,0 +1,91 @@
+# Encoding and decoding methods.
+# Also a collection for dependencies. Probably not a good design idea.
+
+import numpy as np
+import pyaudio
+import math
+
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK = 1024
+
+CHARTIME = 0.05
+min_freq = 220
+log_step = 10
+
+START = 100
+END = 1000
+INC = 30
+WINDOW = (END - START) / INC
+SHIFT = math.floor(WINDOW / 3)
+
+arr = range(100, 1001, 30)
+# 3 unique frequencies per index (lazy coding, could be redone)
+TRIPLES = {i: (arr[3 * i], arr[3 * i + 1], arr[3 * i + 2]) for i in range(WINDOW)}
+# for adjusting power of frequency, placeholder
+POWERS = {i: (1.00, 1.00, 1.00) for i in range(WINDOW)}
+
+
+def char_to_ind(char):
+    if 97 <= ord(char) <= 122:
+        return ord(char) - 97
+    elif char == ' ':
+        return 26
+    else:
+        return 27
+
+def char_to_bin(char):
+    lst = [int(c) for c in np.binary_repr(char_to_ind(char) + 1)]
+    while len(lst) < 5:
+        lst.insert(0, 0)
+    return lst
+
+def ind_to_char(ind):
+    if 0 <= ind < 26:
+        return chr(ind + 97)
+    elif ind == 27:
+        return chr(46)
+    return chr(32)
+
+mappings = {ind_to_char(i): round(min_freq * pow(2, i / log_step), 7) for i in range(28)}
+
+def clean(message):
+    '''
+    Takes in a message and cleans it so that it only has a-z, spaces, and periods.
+    I feel like there's a more efficient implementation of this.
+    '''
+    cleaned_message = []
+    for c in message:
+        if c.isalpha():
+            cleaned_message.append(c.lower())
+        elif not c.isalpha() and c != '.':
+            cleaned_message.append(' ')
+        else:
+            cleaned_message.append('.')
+
+    return ''.join(cleaned_message)
+
+def encode(message, charlength=CHARTIME):
+    '''
+    Encodes a message (str) according to a currently very primitive scheme.
+    Returns a set of tuples (freq, duration) to be passed into 'transmit'.
+    Currently all durations are 0.1 seconds even though that doesn't meet the bitrate.
+    '''
+    return [(mappings[c], charlength) for c in clean(message)]
+
+def new_encode(message):
+    lst = []
+    for _ in range(WINDOW - SHIFT):
+        lst.append(0)
+    for c in clean(message):
+        lst.extend(char_to_bin(c))
+    for _ in range(WINDOW - SHIFT):
+        lst.append(0)
+    return lst
+
+def decode(f):
+    '''
+    Takes in a frequency and returns the best (MAP/MLE eventually?) guess of what the corresponding character is.
+    '''
+    return ind_to_char(int(round(np.log2(f / min_freq) * log_step)))
---
+++ new_sender.py
@@ -0,0 +1,107 @@
+# Sender-side methods.
+
+import pyaudio
+import wave
+import numpy as np
+from encoder import *
+from time import sleep
+from matplotlib import pyplot as plt
+from scipy import signal
+
+window = 3
+
+def start_sending():
+    '''
+    Open the pyaudio stream from the sender side.
+    I essentially took this and play from https://github.com/lneuhaus/pysine/blob/master/pysine/pysine.py
+    '''
+    audio = pyaudio.PyAudio()
+    stream = audio.open(format=pyaudio.paFloat32, channels=1, rate=44100, output=1)
+    return stream, audio
+
+def sine(frequency, length):
+  length = int(length * RATE)
+  factor = float(frequency) * (np.pi * 2) / RATE
+  return np.sin(np.arange(length) * factor)
+
+def harmonics1(freq, length):
+  a = sine(freq * 1.00, length)
+  b = sine(freq * 2.00, length)
+  c = sine(freq * 4.00, length)
+  return (a + b + c) * 0.2
+
+def play_triple(triple, power, length):
+    a = sine(triple[0] * power[0], length)
+    b = sine(triple[1] * power[1], length)
+    c = sine(triple[2] * power[2], length)
+    return (a + b + c)
+
+def get_chunk_for_freqs(freqs, d):
+    return sum([harmonics1(f, d) for f in freqs]) / 3
+
+def get_chunk_for_triples(triples, powers, length):
+    return sum([play_triple(triples[i], powers[i], length)] for i in range(len(triples))) / 3
+
+def get_message_to_play(message, window):
+    '''
+    Takes in a text message and returns the chunk to play.
+    Some inspiration from https://davywybiral.blogspot.com/2010/09/procedural-music-with-pyaudio-and-numpy.html
+    '''
+    encoded = encode(message)
+    duration = 0
+    last_f, next_f = 0, 0
+    chunks = []
+    for i in range(len(encoded)):
+        f, d = encoded[i]
+        if window and i < len(encoded) - 1:
+            next_f = encoded[i + 1][0]
+        if window and i > 0:
+            last_f = encoded[i - 1][0]
+        chunks.append(get_chunk_for_freqs([last_f, f, next_f], d))
+
+    return np.concatenate(chunks)
+
+def new_get_message_to_play(message):
+    encoded = new_encode(message)
+    chunks = []
+    # might have obog right here
+    for i in range(len(encoded) - WINDOW + 1):
+        triples = [TRIPLES[j] for j in range(i, i + WINDOW) if encoded[j]]
+        powers = [POWERS[j] for j in range(i, i + WINDOW) if encoded[j]]
+        chunks.append(get_chunk_for_triples(triples, powers, CHARTIME))
+
+    return np.concatenate(chunks)
+
+
+def play(message, stream, window=False):
+    '''
+    Plays a message as returned by get_message_to_play.
+    '''
+    chunk = get_message_to_play(message, window)
+    stream.write(chunk.astype(np.float32).tostring())
+
+def show_expected_psd(char):
+    '''
+    Takes in a character and plots the PSD we expect to get from hearing it.
+    '''
+    f = mappings[char]
+    chunk = get_chunk_for_freqs([0, f, 0], CHARTIME)
+    f, p = signal.periodogram(chunk, fs=RATE)
+    plt.plot(f[:100], p[:100])
+    plt.title("Character: " + char)
+    plt.show()
+
+def stop_sending(stream, audio):
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+def play_alphabet(stream):
+    return play("abcdefghijklmnopqrstuvwxyz. ", stream)
+
+if __name__ == "__main__":
+    '''for char in "c":
+        show_expected_psd(char)'''
+    stream, audio = start_sending()
+    play("hello world", stream)
+    stop_sending(stream, audio)
