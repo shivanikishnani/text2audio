fe06ae798ad4f820336b968576482e5be9f96cb7
shivanikishnani <shivanik@berkeley.edu>
2019-12-06 04:19:37 -0800

    encode k peaks

--- encode_k.py
+++ encode_k.py
@@ -1,13 +1,21 @@
 import numpy as np
 import pyaudio
 import math
-from encoder import * 
 import pdb
+import doctest
+from scipy.special import comb
 
+"""
+letter messages (str) -> (str2str) windowed messages ->  (str2str) bits -> 
+(str2listofstr_) message_size chunks of bits
+ -> (str_2array) an array of peaks locations corresponding to each chunk
+""" 
 total_freqs = 0
 k_peaks = 0
 time_interval = 0
-message_size = np.log2(total_freqs / min_freq) #per time interval
+message_size = np.log2(comb(total_freqs, k_peaks)) 
+
+
 
 def window_an_array(message_list, trip_window=3, windowing=True):
 	final_list = [[0]*5] * 2
@@ -31,8 +39,72 @@ def window_an_array(message_list, trip_window=3, windowing=True):
 	print(final_list)
 	# final_list.append(message_list[i], final_list.message_list[i + 1])
 	return final_list
-	
 
+def window_str(message, window_size=3, windowing=True):
+	"""
+	str message -> windowed str message
+	
+	>>>window_str("abc")
+	"00a0ababcbc0c00"
+	>>>window_str("a")
+	"00a0a0a00"
+	"""
+	length = len(message)
+	final_message = ""
+
+	if not windowing:
+		window_size = 1
+	else: 
+		message = "00" + message
+		length += 2
+
+	for i in range(length):
+		
+		win = message[i : i + window_size]
+		win = win + "0" * (window_size - len(win))
+
+		final_message += win
+	return final_message
+
+
+def char_to_ind(char):
+    if 97 <= ord(char) <= 122:
+        return ord(char) - 97
+    elif char == ' ':
+        return 26
+    else:
+        return 27
+
+def char_to_bin(char):
+    lst = [str(int(c)) for c in np.binary_repr(char_to_ind(char) + 1)]
+    while len(lst) < 5:
+        lst.extend(str(0))
+    bin_str = ""
+    bin_str = bin_str.join(lst)
+    return bin_str
+
+def ind_to_char(ind):
+    if 0 <= ind < 26:
+        return chr(ind + 97)
+    elif ind == 27:
+        return chr(46)
+    return chr(32)
+
+def clean(message):
+    '''
+    Takes in a message and cleans it so that it only has a-z, spaces, and periods.
+    I feel like there's a more efficient implementation of this.
+    '''
+    cleaned_message = []
+    for c in message:
+        if c.isalpha():
+            cleaned_message.append(c.lower())
+        elif not c.isalpha() and c != '.':
+            cleaned_message.append(' ')
+        else:
+            cleaned_message.append('.')
+
+    return ''.join(cleaned_message)
 
 def encode_peaks(message):
     '''
@@ -40,8 +112,11 @@ def encode_peaks(message):
     onvert message to bits 
     find floor fo log_2 (k)
     '''
-    bin_message = window_an_array([char_to_bin(c) for c in clean(message)])
-
+    window_message = window_str(clean(message))
+    window_bits = "".join([char_to_bin(c) for c in window_message])
+    #divide it into chunks
+    chunk_bits = [window_bits[i:i+message_size] for i in range(0, len(window_bits), message_size)]
+    print(chunk_bits)
  
 
 if __name__ == "__main__":
