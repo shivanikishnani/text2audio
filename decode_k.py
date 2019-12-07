7292b06e909ba48fd3747392fd249819ad4b98f7
shivanikishnani <shivanik@berkeley.edu>
2019-12-06 16:37:54 -0800

    idk

--- decode_k.py
+++ decode_k.py
@@ -21,10 +21,10 @@ def get_peaks(psd_arr, freq_array):
 
 def permutation_into_num(loc_array):
 	F = total_freqs
-    total = 0
-    for i in range(len(loc_array)):
-        total += choose(F - loc_array[i] - 1, len(loc_array) - i)
-    return total
+	total = 0
+	for i in range(len(loc_array)):
+		total += choose(F - loc_array[i] - 1, len(loc_array) - i)
+	return total
 
 
 
@@ -41,73 +41,73 @@ def permutation_into_num(loc_array):
 #SMALL_BLOCK_SIZE * 3. With WINDOWING=False, sound_to_bits can return any number of bits.
 
 def process_sound_from_stream(sound_data, sound_to_bits, WINDOWING=True):
-    global bit_blocks_received
-    global unprocessed_bits
-    bit_blocks_received += sound_to_bits(sound_data)
-    if(WINDOWING):
-        if(len(bit_blocks_received) >= 3):
-            block1 = bit_blocks_received[-3]
-            block2 = bit_blocks_received[-2]
-            block3 = bit_blocks_received[-1]
-            unprocessed_bits += three_blocks_to_one(block1, block2, block3)
-    else:
-        unprocessed_bits += bit_blocks_received[-1]
-    
-    while(len(unprocessed_bits) >= 5):
-        process_five()
+	global bit_blocks_received
+	global unprocessed_bits
+	bit_blocks_received += sound_to_bits(sound_data)
+	if(WINDOWING):
+		if(len(bit_blocks_received) >= 3):
+			block1 = bit_blocks_received[-3]
+			block2 = bit_blocks_received[-2]
+			block3 = bit_blocks_received[-1]
+			unprocessed_bits += three_blocks_to_one(block1, block2, block3)
+	else:
+		unprocessed_bits += bit_blocks_received[-1]
+	
+	while(len(unprocessed_bits) >= 5):
+		process_five()
 
-    print(message_so_far)
+	print(message_so_far)
 
 
 #takes the first five unprocessed bits, translates them, removes them from unprocessed_bits
 #and adds the translated letter to the end of the message so far
 def process_five():
-    global unprocessed_bits
-    global message_so_far
+	global unprocessed_bits
+	global message_so_far
 
-    first_five = unprocessed_bits[0:5]
-    rest = unprocessed_bits[5:]
+	first_five = unprocessed_bits[0:5]
+	rest = unprocessed_bits[5:]
 
-    message_so_far += five_to_letter(first_five)
-    unprocessed_bits = rest
+	message_so_far += five_to_letter(first_five)
+	unprocessed_bits = rest
 
 
 #takes in five bits and returns corresponding letter
 def five_to_letter(bits):
-    n = int(bits, 2)
-    if(n == 28):
-        return '.'
-    if(n == 27):
-        return ' '
-    char = chr(n + 96)
-    if(char in "abcdefghijklmnopqrstuvwxyz"):
-        return char
-    else:
-        return '#'
+	n = int(bits, 2)
+	if(n == 28):
+		return '.'
+	if(n == 27):
+		return ' '
+	char = chr(n + 96)
+	if(char in "abcdefghijklmnopqrstuvwxyz"):
+		return char
+	else:
+		return '#'
 
 
 #takes in three big blocks and returns one small block they all share
 def three_blocks_to_one(block1, block2, block3):
-    end_block1 = block1[SMALL_BLOCK_SIZE * 2:SMALL_BLOCK_SIZE * 3]
-    mid_block2 = block2[SMALL_BLOCK_SIZE: SMALL_BLOCK_SIZE * 2]
-    start_block3 = block3[0: SMALL_BLOCK_SIZE]
-    
-    new_small_block = ""
-    for i in range(SMALL_BLOCK_SIZE):
-        one_count = 0
-        if(end_block1[i] == '1'):
-            one_count += 1
-        if(mid_block2[i] == '1'):
-            one_count += 1
-        if(start_block3[i] == '1'):
-            one_count += 1
-        
-        if(one_count >= 2):
-            new_small_block += '1'
-        else:
-            new_small_block += '0'
-    
-    return new_small_block
+	end_block1 = block1[SMALL_BLOCK_SIZE * 2:SMALL_BLOCK_SIZE * 3]
+	mid_block2 = block2[SMALL_BLOCK_SIZE: SMALL_BLOCK_SIZE * 2]
+	start_block3 = block3[0: SMALL_BLOCK_SIZE]
+	
+	new_small_block = ""
+	for i in range(SMALL_BLOCK_SIZE):
+		one_count = 0
+		if(end_block1[i] == '1'):
+			one_count += 1
+		if(mid_block2[i] == '1'):
+			one_count += 1
+		if(start_block3[i] == '1'):
+			one_count += 1
+		
+		if(one_count >= 2):
+			new_small_block += '1'
+		else:
+			new_small_block += '0'
+	
+	return new_small_block
 
 def convert_num_to_bits(num_for_chars):
 	"""
@@ -118,16 +118,16 @@ def convert_num_to_bits(num_for_chars):
 	return bin_str
 
 def five_to_letter(bits):
-    n = int(bits, 2)
-    if(n == 28):
-        return '.'
-    if(n == 27):
-        return ' '
-    char = chr(n + 96)
-    if (char in "abcdefghijklmnopqrstuvwxyz"):
-        return char
-    else:
-        return '#'
+	n = int(bits, 2)
+	if(n == 28):
+		return '.'
+	if(n == 27):
+		return ' '
+	char = chr(n + 96)
+	if (char in "abcdefghijklmnopqrstuvwxyz"):
+		return char
+	else:
+		return '#'
 
 def decode(psd_array):
 	"""
